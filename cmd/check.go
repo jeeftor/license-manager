ce7e87fbc29744a5ec0045af2151864a602d19a4
Jeff Stein <jeffstein@gmail.com>
2025-01-15 11:34:37 -0700

    Workflow update

--- cmd/check.go
+++ cmd/check.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/yourusername/license-manager/internal/processor"
+	"license-manager/internal/processor"
 )
 
 var checkCmd = &cobra.Command{
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"license-manager/cmd"
 	"os"
 )
 
